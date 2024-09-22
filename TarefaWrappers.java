19e6fe0fd7ccf302f08ab205b84e6ec92f843ca1
Bruno Dambroski <bruno3414.dambroski@gmail.com>
2024-09-22 06:38:46 -0400

    first commit

---
+++ TarefaWrappers.java
@@ -0,0 +1,13 @@
+package br.com.bdam;
+
+public class TarefaWrappers {
+	
+
+	public static void main(String[] args) {
+		int n = 10;
+		System.out.println(n);
+		Short n1 = (short) n;
+		System.out.println(n1);
+	}
+
+}
